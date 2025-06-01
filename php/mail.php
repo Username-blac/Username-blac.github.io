42dc312fc386cd5baacf758afe1e30f16a6ec0e5
Username-blac <358527@weymouth.ac.uk>
2025-06-01 16:10:07 +0100

    Add files via upload

---
+++ php/mail.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = $_POST["fname"] + $_POST["sname"];
+$email = $_POST["email"];
+$number = $_POST["number"];
+$position = $_POST["position"];
+$interview = $_POST["interview"];
+$start_date = $_POST["start"] + $_POST["time"];
+$paragraph = $_POST["summary"];
+$files = $_POST["files"];
